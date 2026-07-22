9753e076709fa90ad0de0efb87b4fb936a717a5c
patryktk <patrick.tkaczyk@gmail.com>
2026-07-22 13:41:15 +0200

    Fix test in scrap service

--- scraper-service/src/test/java/pl/tkaczyk/scraperservice/config/TestAuditingConfig.java
+++ scraper-service/src/test/java/pl/tkaczyk/scraperservice/config/TestAuditingConfig.java
@@ -3,10 +3,12 @@ package pl.tkaczyk.scraperservice.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Optional;
 
 @TestConfiguration
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl", modifyOnCreate = false)
 public class TestAuditingConfig {
 
     @Bean
